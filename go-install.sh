d9eb71b33fc276a8ec8e0150ddb4465782648170
Tan Zhi Yang <80891326+etherealManatee@users.noreply.github.com>
2022-09-08 15:05:25 +0800

    created initial script

---
+++ go-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text)";
+LATEST_GO_DOWNLOAD_URL="https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
+
+wget https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz -P /home/coinhalldev/
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /home/coinhalldev/go1.19.1.linux-amd64.tar.gz
+
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
+source ~/.profile
+echo "export PATH=$PATH:$(go env GOPATH)/bin" >> ~/.profile
