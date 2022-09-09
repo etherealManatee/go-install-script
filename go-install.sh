fcfc7f7d06238414ab3d3459c670eb0979f5d35e
Tan Zhi Yang <80891326+etherealManatee@users.noreply.github.com>
2022-09-09 14:34:55 +0800

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+printf "Checking latest Go version...\n";
 LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text)";
 LATEST_GO_DOWNLOAD_URL="https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
+printf "Latest Go version is ${LATEST_GO_VERSION}.\n"
 
-wget https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz -P /home/coinhalldev/
+printf "cd to $HOME directory\n"
+cd $HOME
+
+printf "Downloading ${LATEST_GO_DOWNLOAD_URL}\n\n";
+wget https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz -P $HOME
+printf "Extracting file and installing Go...\n"
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /home/coinhalldev/go1.19.1.linux-amd64.tar.gz
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> /home/coinhalldev/.profile
-source /home/coinhalldev/.profile
+printf "Updating paths and .profile .../n"
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+source ~/.profile
 echo "export PATH=$PATH:$(go env GOPATH)/bin" >> /home/coinhalldev/.profile
-source /home/coinhalldev/.profile
+source ~/.profile
+printf "Updated!/n"
+go version
